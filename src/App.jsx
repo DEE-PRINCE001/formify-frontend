f394801785622bf0e747fe29f82c14703067aa63
Ajani Honour <ajanihonour@gmail.com>
2026-06-02 11:18:24 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Hero from './components/Hero'
 export default function App() {
   return (
    <div className="bg-primary w-screen h-screen px-15 pt-5">
-     <Navbar></Navbar>
-     <Hero></Hero>
+     <Navbar/>
+     <Hero/>
    </div>
 
   )
